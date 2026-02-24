880ff5519f93ad6532df15f8c5f2dc02b61335a0
tushar102182 <tushar102182>
2026-02-24 06:41:17 +0600

    all about to done

--- scripts/function.js
+++ scripts/function.js
@@ -1,3 +1,41 @@
-function getId(id){
-    return document.getElementById(id);
+
+function view(){
+
+ if(currentStatus ==='all-btn'){
+  cardContainer.classList.remove('hidden');
+  noJobSection.classList.add('hidden');
+  filterSection.classList.add('hidden');
+ }
+ 
+ else if(currentStatus ==='reject-top-btn'){
+  
+  if(rejectedList.length == 0){
+    cardContainer.classList.add('hidden');
+    noJobSection.classList.remove('hidden');
+    filterSection.classList.add('hidden')
+  }
+  else{
+    cardContainer.classList.add('hidden');
+    filterSection.classList.remove('hidden');
+      noJobSection.classList.add('hidden');
+  }
+  renderReject()
+
+ }
+ else if(currentStatus ==='interview-top-btn'){
+  if(interviewList.length == 0){
+    cardContainer.classList.add('hidden');
+    noJobSection.classList.remove('hidden');
+    filterSection.classList.add('hidden');
+  }
+  else{
+    cardContainer.classList.add('hidden');
+    filterSection.classList.remove('hidden');
+     noJobSection.classList.add('hidden');
+  }
+  renderInterview()
+  
+
+ }
+
 }
